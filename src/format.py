ecdafb6a256a5a6b4ad9db98d2eaae5206bb6af0
Kip <49375310+jgkip@users.noreply.github.com>
2022-01-03 18:39:09 -0500

    bold

--- src/format.py
+++ src/format.py
@@ -1,13 +1,37 @@
 import os 
-
+import re
 
 tags = {
         '#' : ('<h1>', '</h1>'),
         '##' : ('<h2>', '</h2>')
         }
 
+def headers(line, h):
+    if line.count('#') == 1:
+        h = tags['#'][0] + line[line.count('#') + 1:].rstrip() + tags['#'][1]
+        #print(header)
+    elif line.count('#') == 2:
+        h = tags['##'][0] + line[line.count('##') + 1:].rstrip() + tags['##'][1]
+        #print(header)
+    return h 
 
+def text_mod(line, h):
+    if bool(re.match(r"__(.*?)__", line)) or bool(re.match(r"[*]{2}(.*?)[*]{2}", line)): #change this to match exactly 2 *
+            h = '<p><b>'+line[2:len(line)-3]+'</b></p>'
+            return h
+    
+    wlist = line.split()
+    for i in range(len(wlist)):
+        if bool(re.match(r"__(.*?)__", wlist[i])) or bool(re.match(r"[*]{2}(.*?)[*]{2}", wlist[i])):  
+            print(wlist[i])
+            wlist[i] = '<b>' + wlist[i][2:len(wlist[i])-2] + '</b>'
 
+    h = ' '.join([str(e) for e in wlist])
+            
+    return h
+
+#TODO: Better way to handle header tags (other tags, too)
+#TODO: italics, bold/italics combo, lists
 def process(name):
     pd = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     pd += '\\tests\\' + name
@@ -19,6 +43,7 @@ def process(name):
     body = ''
     for line in lines: 
         header = ''
+        '''
         #depending on tags, convert to corresponding html tag 
         if line.count('#') == 1:
             header = tags['#'][0] + line[line.count('#') + 1:].rstrip() + tags['#'][1]
@@ -26,9 +51,18 @@ def process(name):
         elif line.count('#') == 2:
             header = tags['##'][0] + line[line.count('##') + 1:].rstrip() + tags['##'][1]
             #print(header)
+        '''
+        if '#' in line:
+            header = headers(line, header)
         elif line.count('#') == 0 and line.isspace() != True:
-            header = '<p>' + line.rstrip() + '</p>'
+            #header = '<p>' + line.rstrip() + '</p>'
             #print(header)
+            if '**' in line or '__' in line:
+                header = text_mod(line, header)
+            else:
+                header = '<p>' + line.rstrip() + '</p>'
+
+
         body += header + '\n'
     
     html = '''
