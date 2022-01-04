97fa039f76db361503ad523dbfd6e2566414e40f
Kip <49375310+jgkip@users.noreply.github.com>
2022-01-03 20:15:24 -0500

    format

--- src/format.py
+++ src/format.py
@@ -6,7 +6,7 @@ tags = {
         '##' : ('<h2>', '</h2>')
         }
 
-def headers(line, h):
+def headers(line):
     if line.count('#') == 1:
         h = tags['#'][0] + line[line.count('#') + 1:].rstrip() + tags['#'][1]
         #print(header)
@@ -15,7 +15,7 @@ def headers(line, h):
         #print(header)
     return h 
 
-def text_mod(line, h):
+def text_mod(line):
     if bool(re.match(r"__(.*?)__", line)) or bool(re.match(r"[*]{2}(.*?)[*]{2}", line)): #change this to match exactly 2 *
             h = '<p><b>'+line[2:len(line)-3]+'</b></p>'
             return h
@@ -23,7 +23,7 @@ def text_mod(line, h):
     wlist = line.split()
     for i in range(len(wlist)):
         if bool(re.match(r"__(.*?)__", wlist[i])) or bool(re.match(r"[*]{2}(.*?)[*]{2}", wlist[i])):  
-            print(wlist[i])
+            
             wlist[i] = '<b>' + wlist[i][2:len(wlist[i])-2] + '</b>'
 
     h = ' '.join([str(e) for e in wlist])
@@ -43,22 +43,12 @@ def process(name):
     body = ''
     for line in lines: 
         header = ''
-        '''
         #depending on tags, convert to corresponding html tag 
-        if line.count('#') == 1:
-            header = tags['#'][0] + line[line.count('#') + 1:].rstrip() + tags['#'][1]
-            #print(header)
-        elif line.count('#') == 2:
-            header = tags['##'][0] + line[line.count('##') + 1:].rstrip() + tags['##'][1]
-            #print(header)
-        '''
         if '#' in line:
-            header = headers(line, header)
-        elif line.count('#') == 0 and line.isspace() != True:
-            #header = '<p>' + line.rstrip() + '</p>'
-            #print(header)
+            header = headers(line)
+        elif line.count('#') == 0 and line.isspace() != True: 
             if '**' in line or '__' in line:
-                header = text_mod(line, header)
+                header = text_mod(line)
             else:
                 header = '<p>' + line.rstrip() + '</p>'
 
